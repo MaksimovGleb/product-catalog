05a6a2000b950ca71cd12b32b2931c4a585aad5d
MaksimovGleb <maksimow.gleb@yandex.ru>
2026-04-25 21:43:28 +0300

    created migrations

---
+++ app/Models/Product.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Product extends Model
+{
+    use HasFactory, SoftDeletes, HasUuids;
+
+    protected $fillable = ['name', 'description', 'price', 'category_id'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
